3d3734ea7369380b07f82434a46903c687c391d7
ashhoobrty <104228671+ashhoobrty@users.noreply.github.com>
2022-05-29 23:35:10 +0530

    Update start.py

--- Plugins/start.py
+++ Plugins/start.py
@@ -5,5 +5,5 @@ from pyrogram import Client as Bot
 @Bot.on_message(filters.command('start'))
 async def start(bot, message):
   await message.reply(
-    text=f"Hello {message.from_user.mention}, I Am A Bot devoloped by Himanshu Rastogi Official.'
+    text=f"Hello {message.from_user.mention}, I Am A Bot devoloped by Himanshu Rastogi Official."
   )
