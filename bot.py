366d9e8cdc51961634cdb9d6ec8bd58718c3441e
ashhoobrty <104228671+ashhoobrty@users.noreply.github.com>
2022-05-29 23:55:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from os import environ
-from pyrogram import Client, filters
+from pyrogram import Client
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
@@ -11,11 +11,5 @@ bot = Client('New Peoject',
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
             )
-
-@bot.on_message(filters.command('start'))
-async def start(bot, message):
-  await message.reply(
-    text=f"Hello {message.from_user.mention}, I Am A Bot devoloped by Himanshu Rastogi Official."
-  )
   
 bot.run()
