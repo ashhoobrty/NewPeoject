c78ec6f69ca22119800faeb459c3a22b128cd905
Jãїиãм <me@jainam.me>
2022-05-29 23:44:33 +0530

    Fixed a small typo

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
 
-bot = Client('New Peoject',
+bot = Client('New Project',
              api_id=API_ID,
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
