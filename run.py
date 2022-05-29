c7540b16a54fddc5ee9a2c157662b0c681ca832d
ashhoobrty <104228671+ashhoobrty@users.noreply.github.com>
2022-05-29 23:13:16 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+from os import environ
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+
+bot = Client('New Peoject',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+            )
+bot.run()
