eacecc38ead1cd2b1d61dd8565497f48e1cba771
ashhoobrty <104228671+ashhoobrty@users.noreply.github.com>
2022-05-29 23:16:41 +0530

    Create start.py

---
+++ Plugins/start.py
@@ -0,0 +1,9 @@
+import pyrogram
+from pyrogram import filters
+from pyrogram import Client as Bot
+
+@Bot.on_message(filters.command('start'))
+async def start(bot, message):
+  await message.reply(
+    text=f"Hello {message.from_user.mention}, I Am A Bot devoloped by Himanshu Rastogi Official.'
+  )
