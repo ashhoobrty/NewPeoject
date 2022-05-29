d6bb566c0b6e787a1bf1e87c4d556f331172c960
ashhoobrty <104228671+ashhoobrty@users.noreply.github.com>
2022-05-29 23:42:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,4 +11,11 @@ bot = Client('New Peoject',
              api_hash=API_HASH,
              bot_token=BOT_TOKEN,
             )
+
+@bot.on_message(filters.command('start'))
+async def start(bot, message):
+  await message.reply(
+    text=f"Hello {message.from_user.mention}, I Am A Bot devoloped by Himanshu Rastogi Official."
+  )
+  
 bot.run()
